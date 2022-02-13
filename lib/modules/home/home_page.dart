a0515719c2f40ae17e4bfe531371a441b0b5ad42
Gabriel Donato <gabrielcdonato@gmail.com>
2022-02-13 03:52:33 -0300

    Feat:(HOME) appbar and bottom navigation

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int indexBottomNavigationBar = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          automaticallyImplyLeading: false, // remove appbar back arrow
+          title: const Text(
+            "News Feed",
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: indexBottomNavigationBar,
+          onTap: (index) => setState(() => indexBottomNavigationBar = index),
+          selectedItemColor: Colors.white,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.backup_table),
+                label: "News",
+                backgroundColor: Colors.black),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.ac_unit),
+              label: "Esports",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.messenger),
+              label: "Social",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_sharp),
+              label: "Profile",
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
